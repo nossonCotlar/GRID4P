b293da94cc15b222d7208e13e362b4b2aa7eda26
nosson <48989814+nossonCotlar@users.noreply.github.com>
2020-12-03 17:03:58 -0600

    Javadoc added

--- src/GRID4P/Gknob.java
+++ src/GRID4P/Gknob.java
@@ -1,16 +1,47 @@
 package grid4p;
 
+/**
+ * The Gknob class is a part of the GRID4P Library.
+ * A Gknob (GRID Knob) is a representation of a modifiable physical parameter
+ * on the Intech GRID Midi Module.
+ * A static Gknob array is handled by the {@link Grid} class,
+ * and as such, Gknob-s needn't be explicitly declared or defined by the user.
+ * That being said, it is welcome and encouraged for the user to explicitly
+ * use Gknob objects, as it makes the program more readable.
+ *
+ * @see Grid
+ * @example BasicWithGknobs
+ */
 public class Gknob {
-    public int val, def;
-    public float min, max;
-    public boolean used, button;
+    protected int val, def;
+    protected float min, max;
+    protected boolean used, button;
 
+    /**
+     * Constructor for a Gknob, intended to be called when populating
+     * an addressable list or array of Gknobs.
+     *
+     * The values for a Gknob default constructor are:
+     *      value: 0
+     *      minimum: 0
+     *      maximum: 127
+     *      button: false
+     */
     Gknob() {
         val = 0;
         min = 0;
         max = 127;
         button = false;
     }
+
+    /**
+     * Constructor for a Gknob, intended to be called when populating
+     * an addressable list or array of Gknobs.
+     *
+     * @param val default value that the Gknob will start off with
+     * @param min minimum value
+     * @param max maximum value
+     */
     Gknob(int val, float min, float max) {
         this.val = val;
         this.min = min;
@@ -19,20 +50,77 @@ public class Gknob {
         used = true;
     }
 
+    /**
+     * Returns the held value, which is the raw integer value mapped
+     * to the range specified by {@link Gknob#min} and {@link Gknob#max}
+     *
+     * @return the value held by the Gknob
+     */
     public float get() {
         return map(val, 0, 127, min, max);
     }
+    /**
+     * Returns the held value, which is the raw integer value mapped
+     * to the range specified by {@link Gknob#min} and {@link Gknob#max}
+     * This is alias of {@link Gknob#get()}
+     *
+     * @return the value held by the Gknob
+     */
     public float value() {
         return this.get();
     }
+    /**
+     * Returns the boolean button value indicating whether or not
+     * the Gknob's button is pushed down.
+     *
+     * @return the button status of the Gknob
+     */
     public boolean getB() {
         return button;
     }
+
+    /**
+     * Returns the <strong>unmapped</strong> value held by the Gknob.
+     * @return the unmapped Gknob value
+     */
+    public int getRaw(){ return this.val;}
+
+    /**
+     * Returns the minimum value that the Gknob can hold.
+     * This normally does not change during runtime and is
+     * primarily for reference and sketch-wide consistency.
+     *
+     * @return the Gknob's maximum value
+     */
     public float min() {return this.min;}
+    /**
+     * Returns the maximum value that the Gknob can hold.
+     * This normally does not change during runtime and is
+     * primarily for reference and sketch-wide consistency.
+     *
+     * @return the Gknob's maximum value
+     */
     public float max() {return this.max;}
+
+    /**
+     * Sets the {@link Gknob#used} flag to false,
+     * indicating this Gknob as vacant for configuration
+     */
     public void free() {
         used = true;
     }
+
+    /**
+     * Method for mapping a value in an existing range to a value in a newly specified range.
+     * This behaves identically to the map() method in the Processing Library.
+     *
+     * @param val value to map
+     * @param ax initial range start point
+     * @param ay initial range end point
+     * @param bx new range start point
+     * @param by new range end point
+     * @return the mapped value
+     */
     private float map(float val, float ax, float ay, float bx, float by){
         return bx + (by - bx) * ((val - ax) / (ay - ax));
     }
--- src/GRID4P/Grid.java
+++ src/GRID4P/Grid.java
@@ -3,7 +3,20 @@ package grid4p;
 import themidibus.*;
 import static java.lang.Integer.min;
 
-public class Grid {
+/**
+ * This is the main class for managing communication with the GRID modules.
+ *
+ * The Grid class <strong>can not</strong> be instantiated, and instead only provides static methods
+ * for assigning parameters, setting configurations, and other functionality.
+ *
+ * The class works hand-in-hand with the Gknob class, which can either be
+ * explicitly or implicitly used by the user.
+ *
+ * @see Gknob
+ * @example Basic
+ */
+
+public abstract class Grid {
     public static final int TOTAL_PARAMS = 64;
     private static final int NOTE_OFFSET = 32;
     private static final int BANK_MULT = 4;
@@ -13,11 +26,15 @@ public class Grid {
     private static int counter; //to keep track of already-used knobs
     private static boolean verbose; //whether to print midi messages to stdout
 
-    public Grid(){
-
-
-    }
-    public static int begin(){
+    /**
+     * The method to initialize the GRID setup.
+     * Does some important tasks like creating an array of Gknobs and
+     * starting the Midi Bus for the actual communication.
+     * This method should be called in the setup() method of a Processing sketch as Grid.begin()
+     *
+     * @example Basic
+     */
+    public static void begin(){
         verbose = false;
         counter = 0;
         k = new Gknob[TOTAL_PARAMS]; //create Gknob array
@@ -26,40 +43,114 @@ public class Grid {
         }
         //MidiBus.list();
         int inputIndex = findIntechInputIndex(MidiBus.availableInputs()); //find midi input with "intech"
-        bus = new MidiBus(); //instanciate up midi bus
+        bus = new MidiBus(); //instantiate up midi bus
         bus.addInput(inputIndex);
         bus.registerParent(new GridCallbackHandler());
-        return 0;
     }
 
-
-    public static float get(int n) { //returns the value of the specified Mknob
-        return k[n].get();
+    /**
+     * Returns the value currently held by the specified Gknob.
+     * not often called, because it is more readable to call it from
+     * the specific Gknob object, but it works just as well.
+     *
+     * @param GknobIndex the index of the GRID's Gknob you wish to access
+     * @return the specified Gknob's value
+     */
+    public static float get(int GknobIndex) { //returns the value of the specified Gknob
+        return k[GknobIndex].get();
     }
-    public static float value(int n) { //alias of get()
-        return k[n].get();
-    }
-    public static boolean getB(int n) { //returns the value of the specified Mknob's button (boolean)
-        return k[n].getB();
+
+    /**
+     * this is an alias of the {@link Grid#get(int) get()} method
+     * Returns the value currently held by the specified Gknob.
+     * not often called, because it is more readable to call it from
+     * the specific Gknob object, but it works just as well.
+     *
+     * @param GknobIndex the index of the GRID's Gknob you wish to access
+     * @return the specified Gknob's value
+     */
+    public static float value(int GknobIndex) { //alias of get()
+        return k[GknobIndex].get();
     }
 
-    public static float getMin(int n) {return k[n].min();}
-    public static float getMax(int n) {return k[n].max();}
+    /**
+     * Returns true or false depending on if the Gknob's button
+     * is currently pushed down.
+     *
+     * @param GknobIndex the index of the GRID's Gknob you wish to access
+     * @return the specified Gknob's button value
+     */
+    public static boolean getB(int GknobIndex) { //returns the value of the specified Gknob's button (boolean)
+        return k[GknobIndex].getB();
+    }
 
-    public static Gknob set(int n, float min, float max) {
+    /**
+     * Returns the minimum value that the specified Gknob can hold.
+     * This normally does not change during runtime and is
+     * primarily for reference and sketch-wide consistency.
+     *
+     * @param GknobIndex the index of the GRID's Gknob you wish to access
+     * @return the specified Gknob's minimum value
+     */
+    public static float getMin(int GknobIndex) {return k[GknobIndex].min();}
+
+    /**
+     * Returns the maximum value that the specified Gknob can hold.
+     * This normally does not change during runtime and is
+     * primarily for reference and sketch-wide consistency.
+     *
+     * @param GknobIndex the index of the GRID's Gknob you wish to access
+     * @return the specified Gknob's maximum value
+     */
+    public static float getMax(int GknobIndex) {return k[GknobIndex].max();}
+
+    /**
+     * Sets the minimum and maximum configuration for the specified Gknob.
+     * Returns the Gknob instance that was configured so that it
+     * may be handled by the user if desired.
+     *
+     * @param GknobIndex the index of the GRID's Gknob you wish to access
+     * @param min the minimum value that the Gknob will hold
+     * @param max the maximum value that the Gknob will hold
+     * @return the newly configured Gknob
+     */
+    public static Gknob set(int GknobIndex, float min, float max) {
         advanceCounter();
-        k[n].min = min;
-        k[n].max = max;
-        k[n].used = true;
-        return k[n];
+        k[GknobIndex].min = min;
+        k[GknobIndex].max = max;
+        k[GknobIndex].used = true;
+        return k[GknobIndex];
     }
-    public static Gknob set(int n, float min, float max, float def) {
-        k[n].val = (int) map(def, min, max, 0, 127);
-        k[n].min = min;
-        k[n].max = max;
-        k[n].used = true;
-        return k[n];
+
+    /**
+     * Sets the minimum, maximum, and default configuration for the specified Gknob.
+     * Returns the Gknob instance that was configured so that it
+     * may be handled by the user if desired.
+     *
+     * @param GknobIndex the index of the GRID's Gknob you wish to access
+     * @param min the minimum value that the Gknob will hold
+     * @param max the maximum value that the Gknob will hold
+     * @param def the default value that the Gknob will begin with
+     * @return the newly configured Gknob
+     */
+    public static Gknob set(int GknobIndex, float min, float max, float def) {
+        k[GknobIndex].val = (int) map(def, min, max, 0, 127);
+        k[GknobIndex].min = min;
+        k[GknobIndex].max = max;
+        k[GknobIndex].used = true;
+        return k[GknobIndex];
     }
+
+    /**
+     * Adds a Gknob configuration to the next vacant Gknob position on the GRID.
+     * Sets the minimum and maximum configuration for said Gknob.
+     * Returns the Gknob instance that was added so that it
+     * may be handled by the user if desired.
+     *
+     * @param min the minimum value that the Gknob will hold
+     * @param max the maximum value that the Gknob will hold
+     * @return the newly added and configured Gknob
+     */
     public static Gknob add(float min, float max) {
         advanceCounter();
         k[counter].min = min;
@@ -67,6 +158,18 @@ public class Grid {
         k[counter].used = true;
         return k[counter];
     }
+
+    /**
+     * Adds a Gknob configuration to the next vacant Gknob position on the GRID.
+     * Sets the minimum, maximum, and default configuration for said Gknob.
+     * Returns the Gknob instance that was added so that it
+     * may be handled by the user if desired.
+     *
+     * @param min the minimum value that the Gknob will hold
+     * @param max the maximum value that the Gknob will hold
+     * @param def the default value that the Gknob will begin with
+     * @return the newly added and configured Gknob
+     */
     public static Gknob add(float min, float max, float def) {
         advanceCounter();
         k[counter].val = (int) map(def, min, max, 0, 127);
@@ -77,38 +180,84 @@ public class Grid {
     }
 
     //Midi Event Handlers
-    public static void controllerChange(int channel, int number, int value) {
+
+    /**
+     * The controllerChange callback for the GridCallbackHandler class
+     * to call once it receives midi input.
+     * Locates the intended Gknob based on the midi channel and
+     * midi number, then inserts the received midi value into {@link Gknob#val}.
+     * Increments the Event Counter,
+     * and calls the {@link Grid#verboseMidiEventMessaging(String, String, String, String, String) verboseMidiEventMessaging} method
+     * providing the appropriate details.
+     *
+     * @param channel midi channel
+     * @param number midi number
+     * @param value midi value
+     */
+    protected static void controllerChange(int channel, int number, int value) {
         int n = min(number - NOTE_OFFSET + channel * BANK_MULT, TOTAL_PARAMS - 1);
         k[n].val = value;
         midiEventCount++;
-        verboseMessaging("Tweak",
+        verboseMidiEventMessaging("Tweak",
                 Integer.toString(channel),
                 Integer.toString(number),
                 Integer.toString(value),
                 Long.toHexString(midiEventCount));
     }
-    public static void noteOn(int channel, int number, int value) {
+
+    /**
+     * The noteOn callback for the GridCallbackHandler class
+     * to call once it receives midi input.
+     * Locates the intended Gknob based on the midi channel and
+     * midi number, then sets the {@link Gknob#button} to true.
+     * Increments the Event Counter,
+     * and calls the {@link Grid#verboseMidiEventMessaging(String, String, String, String, String) verboseMidiEventMessaging} method
+     * providing the appropriate details.
+     *
+     * @param channel midi channel
+     * @param number midi number
+     * @param value midi value
+     */
+    protected static void noteOn(int channel, int number, int value) {
         int n = min(number - NOTE_OFFSET + channel * BANK_MULT, TOTAL_PARAMS - 1);
         k[n].button = true;
         midiEventCount++;
-        verboseMessaging("Note-On",
+        verboseMidiEventMessaging("Note-On",
                 Integer.toString(channel),
                 Integer.toString(number),
                 Integer.toString(value),
                 Long.toHexString(midiEventCount));
     }
-    public static void noteOff(int channel, int number, int value) {
+    /**
+     * The noteOff callback for the GridCallbackHandler class
+     * to call once it receives midi input.
+     * Locates the intended Gknob based on the midi channel and
+     * midi number, then sets the {@link Gknob#button} to false.
+     * Increments the Event Counter,
+     * and calls the {@link Grid#verboseMidiEventMessaging(String, String, String, String, String) verboseMidiEventMessaging} method
+     * providing the appropriate details.
+     *
+     * @param channel midi channel
+     * @param number midi number
+     * @param value midi value
+     */
+    protected static void noteOff(int channel, int number, int value) {
         int n = min(number - NOTE_OFFSET + channel * BANK_MULT, TOTAL_PARAMS - 1);
         k[n].button = false;
         midiEventCount++;
-        verboseMessaging("Note-Off",
+        verboseMidiEventMessaging("Note-Off",
                 Integer.toString(channel),
                 Integer.toString(number),
                 Integer.toString(value),
                 Long.toHexString(midiEventCount));
     }
 
-
+    /**
+     * Sets the {@link Grid#verbose} flag to the supplied value.
+     * If set to true, it also prints the Midi Bus information.
+     *
+     * @param v value for the verbose flag
+     */
     public static void verbose(boolean v) {
         verbose = v;
         if(v){
@@ -121,7 +270,18 @@ public class Grid {
 
         }
     }
-    private static void verboseMessaging(String eventType, String channel, String number, String value, String eventID){
+
+    /**
+     * A generic method for printing the verbosity of Midi Event Messages.
+     * This is intended to be called within the Midi Callback methods.
+     *
+     * @param eventType type of midi event
+     * @param channel midi channel
+     * @param number midi number
+     * @param value midi value
+     * @param eventID midi event ID
+     */
+    private static void verboseMidiEventMessaging(String eventType, String channel, String number, String value, String eventID){
         if(verbose){
             System.out.println(eventType + " | Channel: " +
                     channel + " | Number: " +
@@ -130,14 +290,41 @@ public class Grid {
         }
     }
 
+    /**
+     * Method to advance the vacant Gknob index counter.
+     * Should be called whenever a Gknob is added to an automatically assigned index,
+     * in order not to overwrite previously assigned configurations.
+     * Returns the new counter index position, capped by the {@link Grid#TOTAL_PARAMS}
+     *
+     * @return the new Gknob counter index
+     */
     private static int advanceCounter() {
         while (k[counter].used) counter++;
         return min(counter, TOTAL_PARAMS - 1);
     }
 
+    /**
+     * Method for mapping a value in an existing range to a value in a newly specified range.
+     * This behaves identically to the map() method in the Processing Library.
+     *
+     * @param val value to map
+     * @param ax initial range start point
+     * @param ay initial range end point
+     * @param bx new range start point
+     * @param by new range end point
+     * @return the mapped value
+     */
     private static float map(float val, float ax, float ay, float bx, float by){
         return bx + (by - bx) * ((val - ax) / (ay - ax));
     }
+
+    /**
+     * Auxiliary method for determining the index of the Intech GRID
+     * within the recognized midi inputs
+     *
+     * @param list the list of String identifiers for Midi Inputs
+     * @return the index that corresponds to the Intech GRID
+     */
     private static int findIntechInputIndex(String[] list) {
         for (int i = 0; i < list.length; i++) {
             if (list[i].contains("Intech")) return i;
