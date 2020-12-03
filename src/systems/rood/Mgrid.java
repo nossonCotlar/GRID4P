b3c50e01e0d8ea1dce1a2c74aa3c67e3368bb366
nosson <48989814+nossonCotlar@users.noreply.github.com>
2020-12-02 23:51:30 -0600

    package change

--- src/systems/rood/Mgrid.java
+++
@@ -1,6 +0,0 @@
-//The Mgrid class is deprecated, we're keeping it around for compatibility purposes.
-public class Mgrid extends Grid{
-    Mgrid(){
-        super();
-    }
-}
--- src/systems/rood/Mknob.java
+++
@@ -1,6 +0,0 @@
-//The Mknob class is deprecated, we're keeping it around for compatibility purposes.
-public class Mknob extends Gknob{
-    Mknob(){
-        super();
-    }
-}
