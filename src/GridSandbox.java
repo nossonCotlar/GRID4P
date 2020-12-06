ac26db7ff4f3282b854b7302a296027c6f434ed4
nosson <48989814+nossonCotlar@users.noreply.github.com>
2020-12-06 17:44:46 -0600

    Update GridSandbox.java
    Just a place to test things, not super important guys

--- src/GridSandbox.java
+++ src/GridSandbox.java
@@ -1,2 +1,23 @@
-public class GridSandbox {
+import grid4p.*;
+import processing.core.PApplet;
+
+
+public class GridSandbox extends PApplet {
+    Gknob test;
+     public void settings(){
+        size(500, 500);
+    }
+    public void setup(){
+         Grid.begin();
+         test = new Gknob(0, 500, 250);
+         Grid.add(test);
+    }
+    public void draw(){
+        background(0);
+        circle(width >> 1, height >> 1, test.get());
+    }
+
+    public static void main(String... args){
+        PApplet.main("GridSandbox");
+    }
 }
